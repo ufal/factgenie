ae9b1f3f4793fc406d0f52dacc20c1bc9f5bcf48
Zdenek Kasner <kasnezde@fel.cvut.cz>
2024-09-15 11:50:30 +0200

    Implement aggregated tables and prevalence computation

--- factgenie/analysis.py
+++ factgenie/analysis.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 coloredlogs.install(level="INFO", logger=logger, fmt="%(asctime)s %(levelname)s %(message)s")
 
 
-def load_annotation(line, campaign_id):
+def load_annotations(line, campaign_id):
     j = json.loads(line)
     annotation_records = []
 
@@ -35,6 +35,7 @@ def load_annotation(line, campaign_id):
         "setup_id": j["setup"]["id"],
         "split": j["split"],
     }
+
     for annotation in j["annotations"]:
         r["annotation_type"] = annotation["type"]
         r["annotation_start"] = annotation["start"]
@@ -45,30 +46,59 @@ def load_annotation(line, campaign_id):
     return annotation_records
 
 
-def load_annotations_for_campaigns(campaign_ids):
+def create_example_record(line, campaign_id, annotation_span_categories):
+    # a record is created even if there are no annotations
+    j = json.loads(line)
+
+    example_record = {
+        "annotator_id": j["annotator_id"],
+        "campaign_id": campaign_id,
+        "dataset": j["dataset"],
+        "example_idx": j["example_idx"],
+        "setup_id": j["setup"]["id"],
+        "split": j["split"],
+    }
+
+    for i, category in enumerate(annotation_span_categories):
+        example_record["cat_" + str(i)] = 0
+
+        for annotation in j["annotations"]:
+            if int(annotation["type"]) == i:
+                example_record["cat_" + str(i)] += 1
+
+    return example_record
+
+
+def load_annotations_for_campaign(campaign):
     annotation_index = []
+    example_index = []
 
-    for campaign_id in campaign_ids:
-        jsonl_files = glob.glob(os.path.join(ANNOTATIONS_DIR, campaign_id, "files", "*.jsonl"))
+    campaign_id = campaign.metadata["id"]
+    annotation_span_categories = campaign.metadata["config"]["annotation_span_categories"]
 
-        for jsonl_file in jsonl_files:
-            with open(jsonl_file) as f:
-                lines = f.readlines()
+    jsonl_files = glob.glob(os.path.join(ANNOTATIONS_DIR, campaign_id, "files", "*.jsonl"))
 
-            for line in lines:
-                try:
-                    annotation_records = load_annotation(line, campaign_id)
-                    annotation_index += annotation_records
-                except Exception as e:
-                    logger.error(f"Error while processing line: {line}")
-                    logger.error(e)
+    for jsonl_file in jsonl_files:
+        with open(jsonl_file) as f:
+            lines = f.readlines()
+        for line in lines:
+            try:
+                annotation_records = load_annotations(line, campaign_id)
+                annotation_index += annotation_records
 
-    df = pd.DataFrame(annotation_index)
+                example_record = create_example_record(line, campaign_id, annotation_span_categories)
+                example_index.append(example_record)
+            except Exception as e:
+                logger.error(f"Error while processing line: {line}")
+                logger.error(e)
 
-    return df
+    annotation_index = pd.DataFrame(annotation_index)
+    example_index = pd.DataFrame(example_index)
+
+    return annotation_index, example_index
 
 
-def clean_annotations(df, campaign):
+def preprocess_annotations(df, campaign):
     # remove lines with nans
     df = df.dropna()
 
@@ -89,14 +119,22 @@ def compute_ann_counts(df):
     """
     results = []
 
+    all_annotation_types = df["annotation_type"].unique()
+    all_annotation_types.sort()
+
     for dataset in df["dataset"].unique():
         for split in df["split"].unique():
             for setup_id in df["setup_id"].unique():
                 # filter the dataframe
                 df_filtered = df[(df["dataset"] == dataset) & (df["split"] == split) & (df["setup_id"] == setup_id)]
 
-                # compute the annotation counts
-                ann_counts = df_filtered.groupby("annotation_type").size().reset_index(name="ann_count")
+                # make sure that all annotation types are present in the dataframe, even with zero counts
+                ann_counts = (
+                    df_filtered.groupby("annotation_type")
+                    .size()
+                    .reindex(all_annotation_types, fill_value=0)
+                    .reset_index(name="ann_count")
+                )
 
                 ann_counts["dataset"] = dataset
                 ann_counts["split"] = split
@@ -110,7 +148,7 @@ def compute_ann_counts(df):
     return results
 
 
-def compute_avg_ann_counts(ann_counts, datasets):
+def compute_avg_ann_counts(ann_counts, example_index):
     # for each line in ann_counts, find the corresponding dataset in datasets and add the number of examples
     # then compute the average annotation count
 
@@ -120,54 +158,99 @@ def compute_avg_ann_counts(ann_counts, datasets):
     for i, row in ann_counts.iterrows():
         dataset = row["dataset"]
         split = row["split"]
-        dataset_info = datasets[dataset]
-        ann_counts.loc[i, "example_count"] = int(dataset_info["example_count"][split])
+        setup_id = row["setup_id"]
+        ann_counts.loc[i, "example_count"] = (
+            example_index[
+                (example_index["dataset"] == dataset)
+                & (example_index["split"] == split)
+                & (example_index["setup_id"] == setup_id)
+            ]
+            .example_idx.unique()
+            .shape[0]
+        )
 
     ann_counts["avg_count"] = ann_counts["ann_count"] / ann_counts["example_count"]
+
+    # round to three decimal places
+    ann_counts["avg_count"] = ann_counts["avg_count"].round(3)
+
+    return ann_counts
+
+
+def compute_prevalence(ann_counts, example_index):
+    # for each combination of dataset, split, setup_id, annotation_type, compute the percentage of examples that are affected by the annotation type and add it to the `ann_counts` dataframe
+    for i, row in ann_counts.iterrows():
+        dataset = row["dataset"]
+        split = row["split"]
+        setup_id = row["setup_id"]
+        annotation_type = row["annotation_type"]
+
+        examples = example_index[
+            (example_index["dataset"] == dataset)
+            & (example_index["split"] == split)
+            & (example_index["setup_id"] == setup_id)
+            & (example_index["cat_" + str(annotation_type)] > 0)
+        ]
+
+        ann_counts.loc[i, "prevalence"] = examples.shape[0] / row["example_count"]
+
+        # round to three decimal places
+        ann_counts["prevalence"] = ann_counts["prevalence"].round(3)
 
     return ann_counts
 
 
 def aggregate_ann_counts(ann_counts, groupby):
-    if groupby == "annotation_type":
-        aggregated = ann_counts.groupby("annotation_type").agg({"avg_count": "mean", "ann_count": "sum"}).to_dict()
+    if groupby == "span":
+        aggregated = (
+            ann_counts.groupby("annotation_type")
+            .agg({"avg_count": "mean", "ann_count": "sum", "example_count": "sum", "prevalence": "mean"})
+            .reset_index()
+            .to_dict(orient="records")
+        )
 
-    elif groupby == "setup_id":
+    elif groupby == "setup":
         # keep individual annotation categories, but aggregate setup_ids for each dataset, split
         aggregated = (
-            ann_counts.groupby(["dataset", "split", "annotation_type"])
-            .agg({"avg_count": "mean", "ann_count": "sum"})
-            .to_dict()
+            ann_counts.groupby(["setup_id", "annotation_type"])
+            .agg({"avg_count": "mean", "ann_count": "sum", "example_count": "sum", "prevalence": "mean"})
+            .reset_index()
+            .to_dict(orient="records")
         )
 
     elif groupby == "dataset":
         # keep individual annotation categories, but aggregate datasets for each split, setup_id
         aggregated = (
-            ann_counts.groupby(["split", "setup_id", "annotation_type"])
-            .agg({"avg_count": "mean", "ann_count": "sum"})
-            .to_dict()
+            ann_counts.groupby(["dataset", "split", "annotation_type"])
+            .agg({"avg_count": "mean", "ann_count": "sum", "example_count": "sum", "prevalence": "mean"})
+            .reset_index()
+            .to_dict(orient="records")
         )
+
+    # round to three decimal places
+    for a in aggregated:
+        a["avg_count"] = round(a["avg_count"], 3)
+        a["prevalence"] = round(a["prevalence"], 3)
 
     return aggregated
 
 
 def compute_statistics(app, campaign, datasets):
-    campaign_id = campaign.metadata["id"]
-
     statistics = {}
 
-    df = load_annotations_for_campaigns([campaign_id])
-    df = clean_annotations(df, campaign)
+    annotation_index, example_index = load_annotations_for_campaign(campaign)
+    annotation_index = preprocess_annotations(annotation_index, campaign)
 
-    annotation_counts = compute_ann_counts(df)
-    annotation_counts = compute_avg_ann_counts(annotation_counts, datasets)
+    annotation_counts = compute_ann_counts(annotation_index)
+    annotation_counts = compute_avg_ann_counts(annotation_counts, example_index)
+
+    annotation_counts = compute_prevalence(annotation_counts, example_index)
 
     statistics["ann_counts"] = {
         "full": annotation_counts.to_dict(orient="records"),
-        # "aggregated": {
-        #     "annotation_type": aggregate_ann_counts(ann_counts, "annotation_type"),
-        #     "setup_id": aggregate_ann_counts(ann_counts, "setup_id"),
-        # },
+        "span": aggregate_ann_counts(annotation_counts, "span"),
+        "setup": aggregate_ann_counts(annotation_counts, "setup"),
+        "dataset": aggregate_ann_counts(annotation_counts, "dataset"),
     }
 
     return statistics
--- factgenie/static/js/analyze.js
+++ factgenie/static/js/analyze.js
@@ -1,23 +1,37 @@
 const statistics = window.statistics;
-
 const ann_counts = statistics.ann_counts;
-const ann_counts_full = ann_counts.full;
-
-var table = $('#statistics-table tbody');
 
 $(document).ready(function () {
-    // for each object in ann_counts_full
-    for (const obj of ann_counts_full) {
-        const row = `<tr>
-            <td>${obj.dataset}</td>
-            <td>${obj.split}</td>
-            <td>${obj.setup_id}</td>
-            <td>${obj.annotation_type}</td>
-            <td>${obj.ann_count}</td>
-            <td>${obj.avg_count}</td>
-        </tr>`;
-        table.append(row);
-    }
-    $('#statistics-table').bootstrapTable();
-});
+    function populateTable(tableId, data, columns) {
+        var table = $(`#${tableId} tbody`);
+        table.empty(); // Clear existing rows
 
+        for (const obj of data) {
+            let row = '<tr>';
+            for (const col of columns) {
+                if (col === 'annotation_type') {
+                    row += `<td>
+                        <span class="badge" style="background-color: ${metadata.config.annotation_span_categories[obj[col]].color}; color: rgb(60, 65, 73);">
+                            ${metadata.config.annotation_span_categories[obj[col]].name}
+                        </span>
+                    </td>`;
+                } else {
+                    row += `<td>${obj[col]}</td>`;
+                }
+            }
+            row += '</tr>';
+            table.append(row);
+        }
+        $(`#${tableId}`).bootstrapTable();
+    }
+
+    const fullTableColumns = ['dataset', 'split', 'setup_id', 'example_count', 'annotation_type', 'ann_count', 'avg_count', 'prevalence'];
+    const spanTableColumns = ['example_count', 'annotation_type', 'ann_count', 'avg_count', 'prevalence'];
+    const setupTableColumns = ['setup_id', 'example_count', 'annotation_type', 'ann_count', 'avg_count', 'prevalence'];
+    const datasetTableColumns = ['dataset', 'split', 'example_count', 'annotation_type', 'ann_count', 'avg_count', 'prevalence'];
+
+    populateTable('full-table', ann_counts.full, fullTableColumns);
+    populateTable('span-table', ann_counts.span, spanTableColumns);
+    populateTable('setup-table', ann_counts.setup, setupTableColumns);
+    populateTable('dataset-table', ann_counts.dataset, datasetTableColumns);
+});
