1cb649b3049bd688a05abeba77fddfa448eddc67
Ondrej Platek <ondrej.platek@seznam.cz>
2024-11-05 17:19:26 +0100

    make test to pass

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -2,9 +2,10 @@ def test_demo_ok():
     assert 1 + 1 == 2
 
 
-def test_demo_ko():
-    assert 1 + 1 == 3
-
-
-def test_demo_exception():
-    return 1 / 0
+# # Uncomment to fail the tests
+# def test_demo_ko():
+#     assert 1 + 1 == 3
+#
+#
+# def test_demo_exception():
+#     return 1 / 0
