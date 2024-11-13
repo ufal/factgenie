19c1bd955c8bf5c83515f5f2d7da812cc56af1df
Zdenek Kasner <zdenek.kasner@gmail.com>
2024-11-13 15:53:19 +0100

    Increase version to 1.0.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ install_requires = [
 
 setup(
     name="factgenie",
-    version="1.0.0-1",
+    version="1.0.1",
     python_requires=">=3.9",
     description="Lightweight self-hosted span annotation tool",
     # contributors as on GitHub
