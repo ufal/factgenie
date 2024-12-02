361ece7ca472a8285d57f81da2fbd54514f38128
Zdenek Kasner <zdenek.kasner@gmail.com>
2024-12-02 09:13:55 +0100

    Revert "--amend"
    This reverts commit 9196ed19f8072f8dc5c473c7d31e6697a22eb1a2.

--- factgenie/campaign.py
+++ factgenie/campaign.py
@@ -63,12 +63,8 @@ class Campaign:
         db.to_csv(self.db_path, index=False)
 
     def load_db(self):
-        # no db for external campaigns
-        if self.metadata.get("mode") == CampaignMode.EXTERNAL:
-            self.db = pd.DataFrame()
-            return
-
         dtype_dict = {"annotator_id": str, "start": float, "end": float}
+
         with open(self.db_path) as f:
             self.db = pd.read_csv(f, dtype=dtype_dict)
 
